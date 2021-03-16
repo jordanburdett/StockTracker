252e504844c2e991347116a8edde487c64b3f493
Jordan Burdett <jordan@burdett.us>
2021-03-15 22:34:44 -0600

    forgot npm init

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Tracker = require("./classes/mainTracker");
-const bestbuy = require("./siteWorkers/bestbuyTracker")
 
 console.log(
   "Hello this is the node application that will start Stock Tracker."
--- siteWorkers/bestbuyTracker.js
+++ siteWorkers/bestbuyTracker.js
@@ -1,6 +1,5 @@
 const bby = require("bestbuy")("XyRYR1ggM3t9IqoRU3nSPkWO");
-const sendNotification = require("../utilities/notify")
-const open = require("open");
+const sendNotification = require("../utilities/notify");
 
 const products = "sku=6418599|sku=6439402|sku=6444444|sku=6452940|sku=6442484|sku=6454328|sku=6444449|sku=6446660|sku=6444445|sku=6441172|sku=6442485|sku=6429442|sku=6439299|sku=6439301|sku=6432653|sku=6439300|sku=6432654|sku=6429440|sku=6432400|sku=6436196|sku=6436191|sku=6432655|sku=6432658|sku=6432399|sku=6436194";
 
